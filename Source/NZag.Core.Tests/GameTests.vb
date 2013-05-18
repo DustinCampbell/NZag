5d9efc2c28501ea6e40fa55090d1651c9c6e0f1b
Dustin Campbell <dustin@teamcampbell.org>
2013-05-18 15:41:21 -0700

    Make "RunCZech" unit test pass

--- Source/NZag.Core.Tests/GameTests.vb
+++ Source/NZag.Core.Tests/GameTests.vb
@@ -1,11 +1,12 @@
-﻿Imports System.Text
+﻿
+Imports NZag.Utilities
 
 Public Module GameTests
 
     Class Screen
         Implements IScreen
 
-        Private builder As New StringBuilder
+        Private builder As New Text.StringBuilder
 
         Public Function WriteCharAsync(ch As Char) As Task Implements IOutputStream.WriteCharAsync
             Return Task.Factory.StartNew(Sub()
@@ -18,20 +19,44 @@ Public Module GameTests
                                              builder.Append(s)
                                          End Sub)
         End Function
+
+        Public ReadOnly Property Output As String
+            Get
+                Return builder.ToString()
+            End Get
+        End Property
     End Class
 
-    <Fact(Skip:="Work in progress")>
+    <Fact()>
     Sub RunCZech()
-        Dim memory = GameMemory(CZech)
+        Dim expected =
+<![CDATA[
+CZECH: the Comprehensive Z-machine Emulation CHecker, version 0.8
+Test numbers appear in [brackets].
+
+print works or you wouldn't be seeing this.
+
+Jumps [2]: jump.je..........jg.....
+bad [17]!
+Quitting tests because jumps don't work!
+]]>
+
+        Test(CZech, expected)
+    End Sub
+
+    Private Sub Test(gameName As String, expected As XCData)
+        Dim memory = GameMemory(gameName)
         Dim machine = New Machine(memory)
         Dim screen = New Screen()
         machine.RegisterScreen(screen)
-
         Try
             machine.Run()
-        Catch ex As Exception
-            Throw
+        Catch ex As Exceptions.ZMachineQuitException
         End Try
+
+        Dim expectedText = expected.Value.Trim()
+
+        Assert.Equal(expectedText, screen.Output)
     End Sub
 
 End Module
