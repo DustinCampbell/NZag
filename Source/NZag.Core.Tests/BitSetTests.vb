2f55b67c51c633a85b0cf2578a227dfabc57d191
Dustin Campbell <dustin@teamcampbell.org>
2013-06-12 07:49:01 -0700

    Add more OrWith and RemoveWhere members to IBitSet

--- Source/NZag.Core.Tests/BitSetTests.vb
+++ Source/NZag.Core.Tests/BitSetTests.vb
@@ -2,7 +2,7 @@
 
 Public Module BitSetTests
 
-    Private Sub TestBitSet(bitSet As IBitSet)
+    Private Sub SimpleTests(bitSet As IBitSet)
         ' Verify that IBitSet is initially cleared
         For i = 0 To bitSet.Length - 1
             Assert.Equal(False, bitSet(i))
@@ -76,7 +76,7 @@ Public Module BitSetTests
         Assert.NotNull(bs)
         Assert.Equal("NZag.Utilities.BitSet+BitSet32", bs.GetType().FullName)
 
-        TestBitSet(bs)
+        SimpleTests(bs)
     End Sub
 
     <Fact>
@@ -86,7 +86,7 @@ Public Module BitSetTests
         Assert.NotNull(bs)
         Assert.Equal("NZag.Utilities.BitSet+BitSet64", bs.GetType().FullName)
 
-        TestBitSet(bs)
+        SimpleTests(bs)
     End Sub
 
     <Fact>
@@ -96,7 +96,108 @@ Public Module BitSetTests
         Assert.NotNull(bs)
         Assert.Equal("NZag.Utilities.BitSet+BitSetN", bs.GetType().FullName)
 
-        TestBitSet(bs)
+        SimpleTests(bs)
+    End Sub
+
+    Private Sub OrWithTests(bitSet1 As IBitSet, bitSet2 As IBitSet)
+        Dim len = bitSet1.Length
+        Dim mid = len \ 2
+
+        bitSet1.Clear()
+        bitSet2.Clear()
+
+        For i = 0 To mid - 1
+            bitSet1(i) = True
+        Next
+
+        For i = mid To len - 1
+            bitSet2(i) = True
+        Next
+
+        bitSet1.OrWith(bitSet2)
+
+        For i = 0 To len - 1
+            Assert.True(bitSet1.Contains(i))
+            Assert.True(bitSet1(i))
+        Next
+
+        bitSet1.Clear()
+        bitSet2.Clear()
+
+        For i = 0 To len - 1 Step 4
+            bitSet1(i) = True
+        Next
+
+        For i = 0 To len - 1 Step 2
+            bitSet2(i) = True
+        Next
+
+        bitSet1.OrWith(bitSet2)
+
+        For i = 0 To len - 1 Step 2
+            Assert.True(bitSet1.Contains(i))
+            Assert.True(bitSet1(i))
+            Assert.False(bitSet1.Contains(i + 1))
+            Assert.False(bitSet1(i + 1))
+        Next
+    End Sub
+
+    <Fact>
+    Sub TestOrWith32()
+        Dim bs1 = BitSet.Create(32)
+        Dim bs2 = BitSet.Create(32)
+
+        OrWithTests(bs1, bs2)
+    End Sub
+
+    <Fact>
+    Sub TestOrWith64()
+        Dim bs1 = BitSet.Create(64)
+        Dim bs2 = BitSet.Create(64)
+
+        OrWithTests(bs1, bs2)
+    End Sub
+
+    <Fact>
+    Sub TestOrWith256()
+        Dim bs1 = BitSet.Create(256)
+        Dim bs2 = BitSet.Create(256)
+
+        OrWithTests(bs1, bs2)
+    End Sub
+
+    Sub RemoveWhereTests(bitSet As IBitSet)
+
+        bitSet.Clear()
+
+        For i = 0 To bitSet.Length - 1
+            bitSet(i) = True
+        Next
+
+        bitSet.RemoveWhere(Function(i, v) i Mod 2 = 0)
+
+        For i = 0 To bitSet.Length - 1
+            If i Mod 2 = 0 Then
+                Assert.False(bitSet(i))
+            Else
+                Assert.True(bitSet(i))
+            End If
+        Next
+    End Sub
+
+    <Fact>
+    Sub TestRemoveWhere32()
+        RemoveWhereTests(BitSet.Create(32))
+    End Sub
+
+    <Fact>
+    Sub TestRemoveWhere64()
+        RemoveWhereTests(BitSet.Create(64))
+    End Sub
+
+    <Fact>
+    Sub TestRemoveWhere256()
+        RemoveWhereTests(BitSet.Create(256))
     End Sub
 
 End Module
--- Source/NZag.Core/BitSet.fs
+++ Source/NZag.Core/BitSet.fs
@@ -1,11 +1,16 @@
 ﻿namespace NZag.Utilities
 
+open System
+
 type IBitSet =
     abstract member Add : bit:int -> unit
     abstract member Remove : bit:int -> unit
+    abstract member RemoveWhere : predicate:Func<int,bool,bool> -> unit
     abstract member Clear : unit -> unit
 
     abstract member Contains : bit:int -> bool
+
+    abstract member OrWith : other:IBitSet -> unit
 
     abstract member Item : index:int -> bool with get, set
 
@@ -13,82 +18,175 @@ type IBitSet =
 
 module BitSet =
 
-    let inline private mask32 bit =
-        if bit < 0 || bit > 31 then argOutOfRange "bit" "bit must be in the range 0 to 31."
-        1ul <<< bit
+    let inline private validateBit bit length =
+        if bit < 0 || bit >= length then
+            argOutOfRange "bit" "bit must be in the range 0 to %d." length
 
-    let inline private mask64 bit =
-        if bit < 0 || bit > 63 then argOutOfRange "bit" "bit must be in the range 0 to 63."
-        1UL <<< bit
+    let inline private validateBitSetLength (bitSet: IBitSet) length =
+        if length <> bitSet.Length then
+            argOutOfRange "other" "Bit sets must have the same length. Expected %d but was %d" length bitSet.Length
+
+    let inline private mask32 bit = 1ul <<< bit
+    let inline private mask64 bit = 1UL <<< bit
+
+    type private IBitSet32 =
+        abstract member UnderlyingValue : uint32
 
     type private BitSet32(length: int) =
-        let mutable value = 0ul
+        [<Literal>]
+        let zero = 0ul
 
-        let add bit = value <- value ||| (mask32 bit)
-        let remove bit = value <- value &&& ~~~(mask32 bit)
-        let clear() = value <- 0ul
-        let contains bit = (value &&& (mask32 bit)) <> 0ul
-
-        interface IBitSet with
-            member x.Add bit = add bit
-            member x.Remove bit = remove bit
-            member x.Clear() = clear()
-            member x.Contains bit = contains bit
-
-            member x.Item
-                with get index = contains index
-                 and set index value =
-                    if value then add index
-                    else remove index
-
-            member x.Length = length
-
-    type private BitSet64(length: int) =
-        let mutable value = 0UL
-
-        let add bit = value <- value ||| (mask64 bit)
-        let remove bit = value <- value &&& ~~~(mask64 bit)
-        let clear() = value <- 0UL
-        let contains bit = (value &&& (mask64 bit)) <> 0UL
-
-        interface IBitSet with
-            member x.Add bit = add bit
-            member x.Remove bit = remove bit
-            member x.Clear() = clear()
-            member x.Contains bit = contains bit
-
-            member x.Item
-                with get index = contains index
-                 and set index value =
-                    if value then add index
-                    else remove index
-
-            member x.Length = length
-
-    type private BitSetN(length: int) =
-        let value = Array.zeroCreate (length / 64)
+        let mutable value = zero
 
         let add bit =
-            let index = bit / 64
-            let bit = bit % 64
-            value.[index] <- value.[index] ||| (mask64 bit)
+            validateBit bit length
+            value <- value ||| (mask32 bit)
+
         let remove bit =
-            let index = bit / 64
-            let bit = bit % 64
-            value.[index] <- value.[index] &&& ~~~(mask64 bit)
+            validateBit bit length
+            value <- value &&& ~~~(mask32 bit)
+
         let clear() =
-            for i = 0 to (length / 64) - 1 do
-                value.[i] <- 0UL
+            value <- zero
+
         let contains bit =
-            let index = bit / 64
-            let bit = bit % 64
-            (value.[index] &&& (mask64 bit)) <> 0UL
+            validateBit bit length
+            (value &&& (mask32 bit)) <> zero
+
+        let removeWhere (predicate: Func<int,bool,bool>) =
+            for i = 0 to length - 1 do
+                if predicate.Invoke(i, contains i) then
+                    remove i
+
+        let orWith other =
+            validateBitSetLength other length
+            value <- value ||| (box other :?> IBitSet32).UnderlyingValue
+
+        interface IBitSet32 with
+            member x.UnderlyingValue = value
 
         interface IBitSet with
             member x.Add bit = add bit
             member x.Remove bit = remove bit
+            member x.RemoveWhere predicate = removeWhere predicate
             member x.Clear() = clear()
             member x.Contains bit = contains bit
+            member x.OrWith other = orWith other
+
+            member x.Item
+                with get index = contains index
+                 and set index value =
+                    if value then add index
+                    else remove index
+
+            member x.Length = length
+
+    type private IBitSet64 =
+        abstract member UnderlyingValue : uint64
+
+    type private BitSet64(length: int) =
+        [<Literal>]
+        let zero = 0UL
+
+        let mutable value = zero
+
+        let add bit =
+            validateBit bit length
+            value <- value ||| (mask64 bit)
+
+        let remove bit =
+            validateBit bit length
+            value <- value &&& ~~~(mask64 bit)
+
+        let clear() =
+            value <- zero
+
+        let contains bit =
+            validateBit bit length
+            (value &&& (mask64 bit)) <> zero
+
+        let removeWhere (predicate: Func<int,bool,bool>) =
+            for i = 0 to length - 1 do
+                if predicate.Invoke(i, contains i) then
+                    remove i
+
+        let orWith other =
+            validateBitSetLength other length
+            value <- value ||| (box other :?> IBitSet64).UnderlyingValue
+
+        interface IBitSet64 with
+            member x.UnderlyingValue = value
+
+        interface IBitSet with
+            member x.Add bit = add bit
+            member x.Remove bit = remove bit
+            member x.RemoveWhere predicate = removeWhere predicate
+            member x.Clear() = clear()
+            member x.Contains bit = contains bit
+            member x.OrWith other = orWith other
+
+            member x.Item
+                with get index = contains index
+                 and set index value =
+                    if value then add index
+                    else remove index
+
+            member x.Length = length
+
+    type private IBitSetN =
+        abstract member UnderlyingValue : uint64[]
+
+    type private BitSetN(length: int) =
+        [<Literal>]
+        let zero = 0UL
+        [<Literal>]
+        let resolution = 64
+
+        let byteCount = length / resolution
+        let value = Array.zeroCreate byteCount
+
+        let add bit =
+            validateBit bit length
+            let index = bit / resolution
+            let bit = bit % resolution
+            value.[index] <- value.[index] ||| (mask64 bit)
+
+        let remove bit =
+            validateBit bit length
+            let index = bit / resolution
+            let bit = bit % resolution
+            value.[index] <- value.[index] &&& ~~~(mask64 bit)
+
+        let clear() =
+            Array.clear value
+
+        let contains bit =
+            validateBit bit length
+            let index = bit / resolution
+            let bit = bit % resolution
+            (value.[index] &&& (mask64 bit)) <> zero
+
+        let removeWhere (predicate: Func<int,bool,bool>) =
+            for i = 0 to length - 1 do
+                if predicate.Invoke(i, contains i) then
+                    remove i
+
+        let orWith other =
+            validateBitSetLength other length
+            let otherValue = (box other :?> IBitSetN).UnderlyingValue
+            for i = 0 to byteCount - 1 do
+                value.[i] <- value.[i] ||| otherValue.[i]
+
+        interface IBitSetN with
+            member x.UnderlyingValue = value
+
+        interface IBitSet with
+            member x.Add bit = add bit
+            member x.Remove bit = remove bit
+            member x.RemoveWhere predicate = removeWhere predicate
+            member x.Clear() = clear()
+            member x.Contains bit = contains bit
+            member x.OrWith other = orWith other
 
             member x.Item
                 with get index = contains index
