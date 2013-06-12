4a03c128cc0742ce611937b90ee37183f9aa1d81
Dustin Campbell <dustin@teamcampbell.org>
2013-06-12 07:05:25 -0700

    Add simple BitSet with unit tests

---
+++ Source/NZag.Core.Tests/BitSetTests.vb
@@ -0,0 +1,102 @@
+﻿Imports NZag.Utilities
+
+Public Module BitSetTests
+
+    Private Sub TestBitSet(bitSet As IBitSet)
+        ' Verify that IBitSet is initially cleared
+        For i = 0 To bitSet.Length - 1
+            Assert.Equal(False, bitSet(i))
+        Next
+
+        ' Add each bit
+        For i = 0 To bitSet.Length - 1
+            bitSet.Add(i)
+        Next
+
+        For i = 0 To bitSet.Length - 1
+            Assert.True(bitSet.Contains(i))
+            Assert.True(bitSet(i))
+        Next
+
+        ' Remove each bit
+        For i = 0 To bitSet.Length - 1
+            bitSet.Remove(i)
+        Next
+
+        For i = 0 To bitSet.Length - 1
+            Assert.False(bitSet.Contains(i))
+            Assert.False(bitSet(i))
+        Next
+
+        ' Set each bit
+        For i = 0 To bitSet.Length - 1
+            bitSet(i) = True
+        Next
+
+        For i = 0 To bitSet.Length - 1
+            Assert.True(bitSet.Contains(i))
+            Assert.True(bitSet(i))
+        Next
+
+        ' Clear each bit
+        For i = 0 To bitSet.Length - 1
+            bitSet(i) = False
+        Next
+
+        For i = 0 To bitSet.Length - 1
+            Assert.False(bitSet.Contains(i))
+            Assert.False(bitSet(i))
+        Next
+
+        ' Add every other bit
+        For i = 0 To bitSet.Length - 1 Step 2
+            bitSet.Add(i)
+        Next
+
+        For i = 0 To bitSet.Length - 1 Step 2
+            Assert.True(bitSet.Contains(i))
+            Assert.True(bitSet(i))
+            Assert.False(bitSet.Contains(i + 1))
+            Assert.False(bitSet(i + 1))
+        Next
+
+        ' Clear
+        bitSet.Clear()
+
+        For i = 0 To bitSet.Length - 1
+            Assert.False(bitSet.Contains(i))
+            Assert.False(bitSet(i))
+        Next
+    End Sub
+
+    <Fact>
+    Sub Test32Bits()
+        Dim bs = BitSet.Create(32)
+
+        Assert.NotNull(bs)
+        Assert.Equal("NZag.Utilities.BitSet+BitSet32", bs.GetType().FullName)
+
+        TestBitSet(bs)
+    End Sub
+
+    <Fact>
+    Sub Test64Bits()
+        Dim bs = BitSet.Create(64)
+
+        Assert.NotNull(bs)
+        Assert.Equal("NZag.Utilities.BitSet+BitSet64", bs.GetType().FullName)
+
+        TestBitSet(bs)
+    End Sub
+
+    <Fact>
+    Sub Test256Bits()
+        Dim bs = BitSet.Create(256)
+
+        Assert.NotNull(bs)
+        Assert.Equal("NZag.Utilities.BitSet+BitSetN", bs.GetType().FullName)
+
+        TestBitSet(bs)
+    End Sub
+
+End Module
---
+++ Source/NZag.Core/BitSet.fs
@@ -0,0 +1,110 @@
+﻿namespace NZag.Utilities
+
+type IBitSet =
+    abstract member Add : bit:int -> unit
+    abstract member Remove : bit:int -> unit
+    abstract member Clear : unit -> unit
+
+    abstract member Contains : bit:int -> bool
+
+    abstract member Item : index:int -> bool with get, set
+
+    abstract member Length : int
+
+module BitSet =
+
+    let inline private mask32 bit =
+        if bit < 0 || bit > 31 then argOutOfRange "bit" "bit must be in the range 0 to 31."
+        1ul <<< bit
+
+    let inline private mask64 bit =
+        if bit < 0 || bit > 63 then argOutOfRange "bit" "bit must be in the range 0 to 63."
+        1UL <<< bit
+
+    type private BitSet32(length: int) =
+        let mutable value = 0ul
+
+        let add bit = value <- value ||| (mask32 bit)
+        let remove bit = value <- value &&& ~~~(mask32 bit)
+        let clear() = value <- 0ul
+        let contains bit = (value &&& (mask32 bit)) <> 0ul
+
+        interface IBitSet with
+            member x.Add bit = add bit
+            member x.Remove bit = remove bit
+            member x.Clear() = clear()
+            member x.Contains bit = contains bit
+
+            member x.Item
+                with get index = contains index
+                 and set index value =
+                    if value then add index
+                    else remove index
+
+            member x.Length = length
+
+    type private BitSet64(length: int) =
+        let mutable value = 0UL
+
+        let add bit = value <- value ||| (mask64 bit)
+        let remove bit = value <- value &&& ~~~(mask64 bit)
+        let clear() = value <- 0UL
+        let contains bit = (value &&& (mask64 bit)) <> 0UL
+
+        interface IBitSet with
+            member x.Add bit = add bit
+            member x.Remove bit = remove bit
+            member x.Clear() = clear()
+            member x.Contains bit = contains bit
+
+            member x.Item
+                with get index = contains index
+                 and set index value =
+                    if value then add index
+                    else remove index
+
+            member x.Length = length
+
+    type private BitSetN(length: int) =
+        let value = Array.zeroCreate (length / 64)
+
+        let add bit =
+            let index = bit / 64
+            let bit = bit % 64
+            value.[index] <- value.[index] ||| (mask64 bit)
+        let remove bit =
+            let index = bit / 64
+            let bit = bit % 64
+            value.[index] <- value.[index] &&& ~~~(mask64 bit)
+        let clear() =
+            for i = 0 to (length / 64) - 1 do
+                value.[i] <- 0UL
+        let contains bit =
+            let index = bit / 64
+            let bit = bit % 64
+            (value.[index] &&& (mask64 bit)) <> 0UL
+
+        interface IBitSet with
+            member x.Add bit = add bit
+            member x.Remove bit = remove bit
+            member x.Clear() = clear()
+            member x.Contains bit = contains bit
+
+            member x.Item
+                with get index = contains index
+                 and set index value =
+                    if value then add index
+                    else remove index
+
+            member x.Length = length
+
+    [<CompiledName("Create")>]
+    let create length =
+        if length <= 0 then argOutOfRange "length" "length must be greater than zero."
+
+        if length <= 32 then
+            new BitSet32(length) :> IBitSet
+        elif length <= 64 then
+            new BitSet64(length) :> IBitSet
+        else
+            new BitSetN(length) :> IBitSet
