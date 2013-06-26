f388ed4a818d057cc99e56c497e4bd0c159a4b60
Dustin Campbell <dustin_campbell@outlook.com>
2013-06-26 09:31:00 +0200

    Add reset game command and ensure that the Game tab is selected when the Profile button is unchecked

--- Source/NZag/ViewModels/MainWindowViewModel.cs
+++ Source/NZag/ViewModels/MainWindowViewModel.cs
@@ -73,8 +73,9 @@ namespace NZag.ViewModels
 
             this.OpenGameCommand = RegisterCommand("Open", "Open", OpenGameExecuted, CanOpenGameExecute, new KeyGesture(Key.O, ModifierKeys.Control));
             this.LoadScriptCommand = RegisterCommand("Load Script...", "LoadScript", LoadScriptExecuted, CanLoadScriptExecute);
-            this.PlayGameCommand = RegisterCommand("Play", "Play", PlayGameExecuted, CanPlayGameExecute, new KeyGesture(Key.F5));
             this.ProfileCommand = RegisterCommand<bool>("Profile", "Profile", ProfileExecuted, CanProfileExecute);
+            this.PlayGameCommand = RegisterCommand("Play", "Play", PlayGameExecuted, CanPlayGameExecute, new KeyGesture(Key.F5));
+            this.ResetGameCommand = RegisterCommand("Reset", "Reset", ResetGameExecuted, CanResetGameExecute);
         }
 
         private void OnGameOpened(object sender, EventArgs e)
@@ -86,6 +87,18 @@ namespace NZag.ViewModels
         private void OnScriptLoaded(object sender, EventArgs e)
         {
             this.PropertyChanged("ScriptName");
+        }
+
+        private void StartGame()
+        {
+            if (this.profilingEnabled)
+            {
+                this.gameService.StartGame(this.screenViewModel, this.profilerViewModel);
+            }
+            else
+            {
+                this.gameService.StartGame(this.screenViewModel);
+            }
         }
 
         private bool CanOpenGameExecute()
@@ -131,23 +144,6 @@ namespace NZag.ViewModels
             }
         }
 
-        private bool CanPlayGameExecute()
-        {
-            return this.gameService.IsGameOpen;
-        }
-
-        private void PlayGameExecuted()
-        {
-            if (this.profilingEnabled)
-            {
-                this.gameService.StartGame(this.screenViewModel, this.profilerViewModel);
-            }
-            else
-            {
-                this.gameService.StartGame(this.screenViewModel);
-            }
-        }
-
         private bool CanProfileExecute(bool enabled)
         {
             return true;
@@ -158,9 +154,38 @@ namespace NZag.ViewModels
             this.profilingEnabled = enabled;
         }
 
+        private bool CanPlayGameExecute()
+        {
+            return this.gameService.IsGameOpen;
+        }
+
+        private void PlayGameExecuted()
+        {
+            StartGame();
+        }
+
+        private bool CanResetGameExecute()
+        {
+            return this.gameService.IsGameOpen;
+        }
+
+        private void ResetGameExecuted()
+        {
+            var gameFileName = this.gameService.GameFileName;
+            var scriptFileName = this.gameService.ScriptFileName;
+
+            this.gameService.CloseGame();
+
+            this.gameService.OpenGame(gameFileName);
+            this.gameService.LoadScript(scriptFileName);
+
+            StartGame();
+        }
+
         public ICommand OpenGameCommand { get; private set; }
         public ICommand LoadScriptCommand { get; private set; }
-        public ICommand PlayGameCommand { get; private set; }
         public ICommand ProfileCommand { get; private set; }
+        public ICommand PlayGameCommand { get; private set; }
+        public ICommand ResetGameCommand { get; private set; }
     }
 }
